8c1456bc163762e6bcbecf8849ba0ceca433b90e
Dion Kadriu <dik00028@students.stir.ac.uk>
2021-02-11 10:40:11 -0700

    1: Html class works 2: The uppercase class works 3: comments 4:arguments passed

--- FormatNames.java
+++ FormatNames.java
@@ -8,21 +8,21 @@ public class FormatNames {
 
         Scanner scanner = new Scanner(System.in);
 
-        Formatter uFormatter = new Formatter_u();// the formater for Upper case display
-        Formatter hFormatter = new Formatter_h();// the formater for html display
+        Formatter uFormatter = new FormatterU();// the formater for Upper case display
+        Formatter hFormatter = new FormatterHTML();// the formater for html display
         String file;
         System.out.println("enter the file name");
-        file = scanner.nextLine();
+        file = scanner.nextLine();//get the file name and pass it to the corresponding class
         System.out.println("enter the other file name");
-        String output = scanner.nextLine();
+        String output = scanner.nextLine();//get the ouput name and pass it to the corresponding class
 
 
-        if (args[0].equals("-u")) {
-            uFormatter.inputTaker(file);
+        if (args[0].equals("-u")) {// arguments passed
+            uFormatter.inputTaker(file); //if -u go to this formatter
             uFormatter.outputMaker(output);
         }
-        if (args[0].equals("-h")) {
-            hFormatter.inputTaker(file);
+        if (args[0].equals("-h")) { //arguments passed
+            hFormatter.inputTaker(file);// if -h go to this Formatter
             hFormatter.outputMaker(output);
         }
 
